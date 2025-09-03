664f2a90d258878d5d874903c1e38b3a6a132fc8
Alejoho <alejoreilly96@gmail.com>
2025-09-03 16:45:56 -0400

    Create The FoodList Page

---
+++ BlazorWASMDemoApp/Models/FoodModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace BlazorWASMDemoApp.Models;
+
+public class FoodModel
+{
+    public int Id { get; set; }
+    [DisplayName("Meal")]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
